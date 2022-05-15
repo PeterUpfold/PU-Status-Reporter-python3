fa5d2ed25f63c0da9b63a3ef4f5ea853ee0af985
PeterUpfold <pgithub@upfold.org.uk>
2022-05-15 15:14:05 +0100

    Make the endpoint a const

--- reporter.py
+++ reporter.py
@@ -1,9 +1,12 @@
 
 
-import requests
-import json
 import sys
 import platform
+import json
+import requests
+
+BASE_URI = 'https://apps.upfold.org.uk/StatusReporter'
+#BASE_URI = 'http://127.0.0.1:5001'
 
 def get_headers(key):
     """Return a set of headers for use with requests made to the StatusReporter API."""
@@ -15,21 +18,21 @@ def get_headers(key):
 
 def get_status(context, key):
     """Get the status of the specified context."""
-    req = requests.get('https://apps.upfold.org.uk/StatusReporter/status/' + context, headers=get_headers(key))
+    req = requests.get(BASE_URI + '/status/' + context, headers=get_headers(key))
     if req.status_code != 200:
         raise IOError(f'Getting status for context {context} failed with error code {req.status_code}')
     return req.json()
 
 def get_contexts(key):
     """Return the JSON of all available contexts."""
-    req = requests.get('https://apps.upfold.org.uk/StatusReporter/context', headers=get_headers(key))
+    req = requests.get(BASE_URI + '/context', headers=get_headers(key))
     if req.status_code != 200:
         raise IOError(f'Getting all contexts failed with error code {req.status_code}')
     return req.json()
 
 def get_context(context, key):
     """Check if a given context exists."""
-    req = requests.get('https://apps.upfold.org.uk/StatusReporter/context/' + context, headers=get_headers(key))
+    req = requests.get(BASE_URI + '/context/' + context, headers=get_headers(key))
     if req.status_code == 200:
         return True
     elif req.status_code == 404:
@@ -45,17 +48,17 @@ def create_context(context, key):
         'name': context
     }
 
-    req = requests.put('https://apps.upfold.org.uk/StatusReporter/context', headers=headers, json=data)
+    req = requests.put(BASE_URI + '/context', headers=headers, json=data)
 
     assert req.status_code == 200
 
 def set_status(context, payload, key):
     """Set the status for the specified context to the given payload."""
     data = {
-        'payload': payload   
+        'payload': payload  
     }
 
-    req = requests.post('https://apps.upfold.org.uk/StatusReporter/status/' + context, headers=get_headers(key), json=data)
+    req = requests.post(BASE_URI + '/status/' + context, headers=get_headers(key), json=data)
     if req.status_code != 200:
         raise IOError(f'Setting status for context {context} failed with error code {req.status_code}')
     return req.json()
